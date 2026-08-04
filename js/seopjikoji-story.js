2e82e2f08c3dfa3edfbbe1a776697148f75f3b74
byeol-lee <byeol.lee@kakao.com>
2026-08-04 16:08:29 +0900

    Add files via upload

--- js/seopjikoji-story.js
+++ js/seopjikoji-story.js
@@ -2,7 +2,7 @@ const locations = [
     {
         title: "섭지코지",
         desc: "아름다운 해안 절경과 독특한 화산 지형을 감상할 수 있는 제주 동부의 해안 명소",
-        detailDesc: "‘섭지’는 재사(재주가 뛰어난 선비)가 많이 배출되는 지세란 뜻이고, '코지'는 코끝처럼 바닷가로 불쑥 튀어나온 땅(곶)을 의미하는 제주 방언입니다. 섭지코지는 잎사귀 모양이고, 송이라는 붉은 화산재로 작은 오름이 형성되어 있으며, 썰물과 밀물 사이 거대하거나 혹은 미미한 바위들이 폈다 집니다. 사계절 아름다운 섭지코지는 특히 4월 화려한 유채꽃이 한창으로 가장 아름답습니다. 눈이 부신 유채밭 사이를 거닐면서 섭지코지의 해안 절경과 거대한 성산일출봉을 두 눈에 담아 보세요.",
+        detailDesc: "'섭지'는 재사(재주가 뛰어난 선비)가 많이 배출되는 지세란 뜻이고, '코지'는 코끝처럼 바닷가로 불쑥 튀어나온 땅(곶)을 의미하는 제주 방언입니다. 섭지코지는 잎사귀 모양이고, 송이라는 붉은 화산재로 작은 오름이 형성되어 있으며, 썰물과 밀물 사이 거대하거나 혹은 미미한 바위들이 폈다 집니다. 사계절 아름다운 섭지코지는 특히 4월 화려한 유채꽃이 한창으로 가장 아름답습니다. 눈이 부신 유채밭 사이를 거닐면서 섭지코지의 해안 절경과 거대한 성산일출봉을 두 눈에 담아 보세요.",
         img: "/api/editor/attach/view/default/2026/08/pFJQVGG7HEuQv2lIdX24_1_",
         walkTime: false,
         link: false,
@@ -15,17 +15,17 @@ const locations = [
         detailDesc: "세계적인 건축가 안도 다다오가 설계한 글라스 하우스는 제주의 정동 쪽을 향해 위치해 있는데, 정동향은 태양의 정기를 정면으로 품는 방향으로 태양에 대한 영원한 동경과 이상향을 의미합니다. 글라스 하우스는 동서남북 어느 곳에서도 일출과 일몰의 장관을 볼 수 있는 유리 건물로 건축되었습니다. 극히 단순하지만 절제된 조형미로 건물이라기보다는 하나의 모던하고 절제된 현대 조형물을 보는 듯한 느낌을 불러옵니다.",
         img: "/api/editor/attach/view/default/2026/08/HugWfXInyKzSXaJjXBMQ_1_",
         walkTime: "도보 12분",
-        link: "https://phoenixhnr.co.kr/static/jeju/architecture/glass-house",
-        lat: 33.4251,
-        lng: 126.9304
+        link: "/static/jeju/architecture/glass-house",
+        lat: 33.4291,
+        lng: 126.9339
     },
     {
         title: "유민 아르누보 뮤지엄",
         desc: "아르누보의 유리 공예 작품을 전시하는 미술관",
         detailDesc: "유민 아르누보 뮤지엄은 일본인 건축가 안도 다다오의 노출 콘크리트 건축 작품입니다. 건축물 안에는 프랑스 낭시 지역의 아르누보(1890-1910년) 유리 공예 작품들이 전시되어 있습니다. 뮤지엄은 삼다(三多)의 정원과 벽천폭포를 지나면 들어갈 수 있는 구조입니다. 벽천폭포의 뷰 파인더 창에서 바라보는 성산일출봉의 풍경은 더욱 아름답게 느껴집니다. 뮤지엄의 실내는 고요하고 어두우며, 슬릿 창으로 들어오는 햇살과 함께 빛나는 유리 공예 작품들을 감상하실 수 있습니다.",
-        img: "https://phoenixhnr.co.kr/images/ko/architecture/photo-yumin_artnouveau04.jpg",
+        img: "/images/ko/architecture/photo-yumin_artnouveau04.jpg",
         walkTime: "도보 8분",
-        link: "https://phoenixhnr.co.kr/static/jeju/architecture/yuminart1",
+        link: "/static/jeju/architecture/yuminart1",
         lat: 33.4291,
         lng: 126.9322
     },
@@ -35,7 +35,7 @@ const locations = [
         detailDesc: "세계적인 건축가 마리오 보타가 설계한 아고라는 유리 피라미드 형태의 독특한 건축물로 석양이 질 때 유리에 반짝이는 모습이 장관을 선사합니다. 낮에는 땅 위의 기운과 태양의 기운을 받고, 유리 벽을 통한 섭지코지의 자연이 그대로 투영됩니다. 밤에는 땅 위로 기운을 발산하는 듯한 야간 조명으로 존재감을 드러냅니다. 피라미드의 꼭짓점에 태양이 떠오르면 수정 구슬에 태양의 빛이 투영되어, 건축의 내부는 환상적인 빛으로 채워집니다.",
         img: "/api/editor/attach/view/default/2026/08/ItPYQ29U4eL88cumbfjp_1_",
         walkTime: "도보 3분",
-        link: "https://phoenixhnr.co.kr/static/jeju/architecture/agora",
+        link: "/static/jeju/architecture/agora",
         lat: 33.4259,
         lng: 126.9264
     },      
@@ -46,8 +46,8 @@ const locations = [
         img: "/api/editor/attach/view/default/2026/08/sR5M9NWgu9XV67sZEldX_1_",
         walkTime: "도보 14분",
         link: "#",
-        lat: 33.4304,
-        lng: 126.9336
+        lat: 33.4295,
+        lng: 126.9345
     },
     {
         title: "행복의 문",
@@ -56,8 +56,8 @@ const locations = [
         img: "/api/editor/attach/view/default/2026/08/D1DMNo4tQBsv645MHdO1_1_",
         walkTime: "도보 10분",
         link: "#",
-        lat: 33.4259,
-        lng: 126.9298
+        lat: 33.4296,
+        lng: 126.9285
     },      
     {
         title: "방두포 등대",
@@ -66,8 +66,8 @@ const locations = [
         img: "/api/editor/attach/view/default/2026/08/vAq71auk6JuOK7qCVv57_1_",
         walkTime: "도보 15분",
         link: "https://example.com",
-        lat: 33.4243,
-        lng: 126.9312
+        lat: 33.4274,
+        lng: 126.9336
     },
     {
         title: "올레길 미로",
@@ -160,7 +160,7 @@ kakao.maps.load(function () {
         const isValidWalkTime = loc.walkTime && loc.walkTime !== "false" && loc.walkTime !== false;
         const walkTimeHtml = isValidWalkTime
             ? `<div class="walk-info">
-                <img src="https://phoenixhnr.co.kr/images/ko/experience/ico-address.png" alt="위치 아이콘" class="ico-address">
+                <img src="/images/ko/experience/ico-address.png" alt="위치 아이콘" class="ico-address">
                 <span>B동 기준 ${loc.walkTime}</span>
                </div>`
             : '';
