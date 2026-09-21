bc3e90d8e8a5ce8e62436b21fc57e732692562ac
byeol-lee <byeol.lee@kakao.com>
2026-09-21 15:24:25 +0900

    Update activities.js

--- js/en/island/activities.js
+++ js/en/island/activities.js
@@ -28,7 +28,7 @@ const activities = [
         `,
         priceGuest: `20% off`,
         information: `
-        <p class="ref-bu-list">※ Location: 95, Seopjikoji-ro, Seongsan-eup, Seogwipo-si, Jeju-do, Republic of Koreaㄴ<br />
+        <p class="ref-bu-list">※ Location: 95, Seopjikoji-ro, Seongsan-eup, Seogwipo-si, Jeju-do, Republic of Korea<br />
         (Takes approx. 1-minute drive from Phoenix Island)</p>
         <p class="ref-bu-list">※ Please make your own way to the program venue.</p>
         `,
